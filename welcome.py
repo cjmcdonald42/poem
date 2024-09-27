881ee6bfe550e17121261feca885ad8406bc90ff
Charles J McDonald <cjmcdonald42@gmail.com>
2024-09-27 19:31:54 -0400

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,25 @@
+#
+# Directions:
+#    Find and correct all the errors in this code.
+#    Make changes to this code to make it more elegant and efficient
+#
+#    Note that some 'mistakes' are not technically wrong - they are just not best practices.
+#
+
+print("Welcome to our first GitHub Repository)
+userName = str(input("What is your name?"))
+
+if userName = "Oscar" or "Chuck"
+  print("You are Oscar")
+else :
+  print("You are Chuck!")
+
+# --- Add three numbers
+x = int(4)
+numTwo = int(10)
+ThirdNumber = 7.5
+
+x + numTwo + ThirdNumber = finalAnswer
+
+print("The final answer is finalAnswer")
+
